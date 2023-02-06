bef6d7107b02ebf53c95c3a8a89854754d72622c
Nuongho1603 <nuongho.160304@gmail.com>
2023-02-06 11:32:52 +0700

    up

---
+++ module_5/ss4_.angular_component_template/exercise/angular-hackernews-app/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import {FormsModule} from "@angular/forms";
+import { LikeComponent } from './like/like.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { DeleteComponent } from './delete/delete.component';
+import { DislikeComponent } from './dislike/dislike.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticleComponent,
+    LikeComponent,
+    NavbarComponent,
+    FooterComponent,
+    DeleteComponent,
+    DislikeComponent
+  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
